2ccfd65abbca368c0387305818bda3e061053913
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-05-07 12:36:13 +1000

    Stupid

--- server/migrations/20260228111432-add-default-values-to-player-state.js
+++ server/migrations/20260228111432-add-default-values-to-player-state.js
@@ -3,12 +3,12 @@ const { DataTypes } = require('sequelize');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('player_state', 'created_at', {
+    await queryInterface.changeColumn('player_state', 'createdAt', {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: Sequelize.fn('NOW'),
     });
-    await queryInterface.changeColumn('player_state', 'updated_at', {
+    await queryInterface.changeColumn('player_state', 'updatedAt', {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: Sequelize.fn('NOW'),
@@ -16,11 +16,11 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('player_state', 'created_at', {
+    await queryInterface.changeColumn('player_state', 'createdAt', {
       type: DataTypes.DATE,
       allowNull: false,
     });
-    await queryInterface.changeColumn('player_state', 'updated_at', {
+    await queryInterface.changeColumn('player_state', 'updatedAt', {
       type: DataTypes.DATE,
       allowNull: false,
     });
